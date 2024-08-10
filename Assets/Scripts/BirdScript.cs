0278d47d2504a4d1f1c0dfad203c5b0c72e3b9d3
Salih <sskarakussalih77@gmail.com>
2024-08-11 01:38:05 +0300

    last edit and finished

--- Assets/Scripts/BirdScript.cs
+++ Assets/Scripts/BirdScript.cs
@@ -1,22 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BirdScript : MonoBehaviour
 {
 
+    public Text skor_text;
+    public float skor;
     public float zıplama_aralığı;
 
     Rigidbody2D rb;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        skor = 0;
     }
 
         void Update()
     {
-        if(Input.GetMouseButtonDown(0)){
+        if(Input.GetMouseButtonDown(0))
+        {
         rb.velocity = Vector2.up * zıplama_aralığı;
+        }
+
+        skor_text.text = skor.ToString();
     }
+
+    void OnTriggerEnter2D(Collider2D temas)
+    {
+        if(temas.gameObject.tag == "Scorer")
+        {
+        skor++;
+        }
     }
+
+    void OnCollisionEnter2D(Collision2D temas)
+    {
+        if(temas.gameObject.tag == "Pipe")
+        {
+            Time.timeScale = 0;
+        }
+    }
+
+    
 }
